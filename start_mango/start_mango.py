cbedf0b63c1df721396a6a76088935c34b427566
Samie Mostafavi <ssmos@kth.se>
2023-02-15 17:22:45 +0100

    debug hostapd

--- start_mango/start_mango.py
+++ start_mango/start_mango.py
@@ -3,13 +3,14 @@ import paramiko
 import json
 import socket
 import re
+import time
 
 WAIT_DURATION_SEC=40
 POLLING_INTERVAL=10
 
 # Example:
 # DESIGN='mango' SDR='sdr-01' SIDE='sta' CONFIG='{"mac_addr":"40:d8:55:04:20:19"}' JSON_PATH='../sdrs.json' python start_mango.py
-# DESIGN='mango' SDR='sdr-02' SIDE='ap' CONFIG='{"mac_addr":"40:d8:55:04:20:10"}' JSON_PATH='../sdrs.json' python start_mango.py
+# DESIGN='mango' SDR='sdr-02' SIDE='ap' CONFIG='{"mac_addr":"40:d8:55:04:20:12"}' JSON_PATH='../sdrs.json' python start_mango.py
 
 def check_host(server_ip,port):
     try:
@@ -29,6 +30,28 @@ def check_host(server_ip,port):
         return False
     except socket.error:
         return False
+
+def parse_iwinfo(inp):
+
+    lines = inp.splitlines()
+    res_dict = {}
+    for line in lines:
+        if "," in line:
+            if "(" in line and ")" in line:
+                line = re.sub(r'\([^)]*\)', '', line)
+            sections = line.split(',')
+            sections = [ sec.strip() for sec in sections ]
+            for sec in sections:
+                sec.strip()
+                if ":" in sec:
+                    subsecs = sec.split(":")
+                    subsecs = [ssec.strip() for ssec in subsecs]
+                else:
+                    subsecs = sec.split(" ")
+                res_dict[subsecs[0]] = subsecs[1]
+        else:
+            res_dict[line.split(' ', 1)[0].replace('\t','')] = line.split(' ', 1)[1].replace('\t','')
+    return res_dict
 
 
 def run_cmd_mango(ip,command):
@@ -62,6 +85,41 @@ def run_cmd_mango(ip,command):
 
     return stdout,stderr
 
+def run_cmds_mango(ip,commands):
+
+    # commands example
+    # commands = [
+    #     'cat file.txt',
+    #     'cat file2.txt',
+    # ]
+
+    username = "root"
+    password = "root"
+    print(f"SSHing to {ip}, username:{username}, password: {password}")
+
+    # initialize the SSH client
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=ip, username=username, password=password)
+    except:
+        print("[!] Cannot connect to the SSH Server")
+        exit()
+
+    # execute the command
+    for command in commands:
+        print(f"running command on {ip}: \n{command}")
+        stdin, stdout, stderr = client.exec_command(command)
+        stdout = stdout.read().decode().strip()
+        if stdout:
+            print(f"command stdout: \n{stdout}")
+        stderr = stderr.read().decode().strip()
+        if stderr:
+            print(f"command sdterr: \n{stderr}")
+
+    # close socket
+    if client is not None:
+        client.close()
 
 def check_mango(ip):
 
@@ -104,22 +162,6 @@ def check_mango(ip):
         )
         status["wlan0_ip"] = stdout
 
-    # check wlan0 link if it is STA and up
-    if status["wlan0_up"] and status["wlan0_info"]["type"] == "managed":
-        stdout, stderr = run_cmd_mango(
-            ip,
-            '/usr/sbin/iw dev wlan0 link',
-        )
-        status["wlan0_link"] = parse_iwlink(stdout)
-
-    # check wlan0 stations if it is AP and up
-    if status["wlan0_up"] and status["wlan0_info"]["type"].lower() == "ap":
-        stdout, stderr = run_cmd_mango(
-            ip,
-            '/usr/sbin/iw dev wlan0 station dump',
-        )
-        status["wlan0_link"] = parse_iwstationdump(stdout)
-
     return status
 
 
@@ -160,35 +202,47 @@ if __name__ == "__main__":
 
         # modeprobe mango_wlan
         stdout, stderr = run_cmd_mango(
-            ip,
+            sdr_status['mango']['ip'],
             '/sbin/modprobe mango_wlan',
         )
 
+
         # delete iw device
         stdout, stderr = run_cmd_mango(
-            ip,
+            sdr_status['mango']['ip'],
             '/usr/sbin/iw wlan0 del',
         )
 
         # create a new interface with the new mac_addr
         stdout, stderr = run_cmd_mango(
-            ip,
+            sdr_status['mango']['ip'],
             f'/usr/sbin/iw phy mango-wlan-phy interface add wlan0 type managed addr {wifi_conf["mac_addr"]}',
         )
 
     else:
-        print(f"{sdr} already has wlan0, it is advised reboot it.")
+        print(f"{sdr} already has wlan0, it is advised to reboot it.")
 
     # ready to run the iw commands
     if side == 'sta':
         stdout, stderr = run_cmd_mango(
-            ip,
+            sdr_status['mango']['ip'],
             '/home/root/run_sta.sh',
         )
+
+
     elif side == 'ap':
-        stdout, stderr = run_cmd_mango(
-            ip,
-            '/home/root/run_ap.sh',
+        run_cmds_mango(
+            sdr_status['mango']['ip'],
+            [
+                "/usr/bin/killall dnsmasq",
+                "/usr/sbin/hostapd -B /home/root/hostapd.conf",
+                "/bin/sleep 10",
+                "/sbin/ifconfig wlan0 up 192.168.11.1 netmask 255.255.255.0",
+                "/sbin/route add -net 192.168.11.0 netmask 255.255.255.0 gw 192.168.11.1",
+                "/usr/bin/dnsmasq --interface=wlan0 --except-interface=lo --bind-interfaces --dhcp-range=192.168.11.2,192.168.11.30,255.255.255.0,12h --conf-file=/dev/null",
+                "/usr/sbin/iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE",
+                "/bin/echo 1 > /proc/sys/net/ipv4/ip_forward",
+            ]
         )
 
     # Wait for the new design
@@ -198,7 +252,7 @@ if __name__ == "__main__":
     while time.time() < t_end:
         print(f"{POLLING_INTERVAL} seconds to the next poll...")
         time.sleep(POLLING_INTERVAL)
-        check_dict = check_mango(ip)
+        check_dict = check_mango(sdr_status['mango']['ip'])
         if check_dict['wlan0_ip']:
             success = True
             break
@@ -207,16 +261,4 @@ if __name__ == "__main__":
         print(f'{sdr} mango is running {side} with ip {check_dict["wlan0_ip"]}.')
     else:
         print(f'{sdr} mango start {side} did not work.')
-
-    return success
-
-
-
-    # if wlan0 is up, read the ip
-    if status["wlan0_up"]:
-        stdout, stderr = run_cmd_mango(
-            ip,
-            "/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2| cut -d' ' -f1",
-        )
-        status["wlan0_ip"] = stdout
 
