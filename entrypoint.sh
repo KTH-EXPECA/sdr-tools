79d844d0f17098e652e144342c45ce5c6f159992
Samie Mostafavi <ssmos@kth.se>
2023-05-12 18:13:05 +0200

    add check_mango to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,21 @@
 if [[ "$SERVICE" == "reboot" ]]
 then
   sh -c 'python3 /service/reboot/reboot.py'
+  sh -c 'sleep infinity'
 elif [[ "$SERVICE" == "change_design" ]]
 then
   sh -c 'python3 /service/change_design/change_design.py'
+  sh -c 'sleep infinity'
 elif [[ "$SERVICE" == "start_mango" ]]
 then
   sh -c 'python3 /service/start_mango/start_mango.py'
+  sh -c 'sleep infinity'
+elif [[ "$SERVICE" == "check_mango" ]]
+then
+  sh -c 'python3 /service/check_mango/check_mango.py'
+  sh -c 'sleep infinity'
 elif [[ "$SERVICE" == "config_mango_routes" ]]
 then
   sh -c 'python3 /service/config_mango_routes/config_mango_routes.py'
+  sh -c 'sleep infinity'
 fi
